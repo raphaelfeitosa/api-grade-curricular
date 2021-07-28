64d7071e5c8b1e92e3094733d4a9aa34a12b7536
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-28 13:47:01 -0300

    create exception materia

---
+++ src/main/java/com/cliente/escola/gradecurricular/exceptions/MateriaException.java
@@ -0,0 +1,15 @@
+package com.cliente.escola.gradecurricular.exceptions;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class MateriaException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    private final HttpStatus httpStatus;
+
+    public MateriaException(final String mensagem, final HttpStatus httpStatus) {
+        super(mensagem);
+        this.httpStatus = httpStatus;
+    }
+}
