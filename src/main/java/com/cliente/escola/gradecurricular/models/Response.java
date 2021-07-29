8acfbe868da0c854da4f29230278b049a427a210
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-28 23:26:46 -0300

    create generic response

---
+++ src/main/java/com/cliente/escola/gradecurricular/models/Response.java
@@ -0,0 +1,17 @@
+package com.cliente.escola.gradecurricular.models;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.hateoas.RepresentationModel;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class Response<T> extends RepresentationModel<Response<T>> {
+    private int statusCode;
+    private T data;
+    private long timeStamp;
+
+    public Response(){
+        this.timeStamp = System.currentTimeMillis();
+    }
+}
