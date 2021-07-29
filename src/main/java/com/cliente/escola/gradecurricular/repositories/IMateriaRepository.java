248f6e6c67a67f013fe2c3022eb0fc516a8151b8
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-29 13:47:47 -0300

    add filter with frequencia and horario minimo

--- src/main/java/com/cliente/escola/gradecurricular/repositories/IMateriaRepository.java
+++ src/main/java/com/cliente/escola/gradecurricular/repositories/IMateriaRepository.java
@@ -2,6 +2,15 @@ package com.cliente.escola.gradecurricular.repositories;
 
 import com.cliente.escola.gradecurricular.entities.MateriaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface IMateriaRepository extends JpaRepository<MateriaEntity, Long> {
+    @Query("SELECT m FROM MateriaEntity m WHERE m.horas >= :horaMinima")
+    public List<MateriaEntity> findByHorarioMinimo(@Param("horaMinima") int horaMinima);
+
+    @Query("SELECT m FROM MateriaEntity m WHERE m.frequencia = :frequencia")
+    public List<MateriaEntity> findByFrequencia(@Param("frequencia") int frequencia);
 }
