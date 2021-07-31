657d830be1bf748d2f7514ad9d93b1636bf9a379
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-30 23:22:06 -0300

    remove public methods

--- src/main/java/com/cliente/escola/gradecurricular/services/IMateriaService.java
+++ src/main/java/com/cliente/escola/gradecurricular/services/IMateriaService.java
@@ -5,11 +5,11 @@ import com.cliente.escola.gradecurricular.dto.MateriaDto;
 import java.util.List;
 
 public interface IMateriaService {
-    public Boolean cadastrarMateria(final MateriaDto materiaDto);
-    public List<MateriaDto> listarMaterias();
-    public MateriaDto consultarMateria(final Long id);
-    public Boolean atualizarMateria(final MateriaDto materiaDto);
-    public Boolean excluirMateria(final Long id);
-    public List<MateriaDto> listarMateriasPorHorarioMinimo(int horaMinima);
-    public List<MateriaDto> listarMateriasPorFrequencia(int frequencia);
+     Boolean cadastrarMateria(final MateriaDto materiaDto);
+     List<MateriaDto> listarMaterias();
+     MateriaDto consultarMateria(final Long id);
+     Boolean atualizarMateria(final MateriaDto materiaDto);
+     Boolean excluirMateria(final Long id);
+     List<MateriaDto> listarMateriasPorHorarioMinimo(int horaMinima);
+     List<MateriaDto> listarMateriasPorFrequencia(int frequencia);
 }
