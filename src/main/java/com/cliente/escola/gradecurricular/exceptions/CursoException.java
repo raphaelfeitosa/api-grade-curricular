10149afd9467632382a17f13b10b8d0d48d3c07e
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-30 23:51:11 -0300

    create exception curso

---
+++ src/main/java/com/cliente/escola/gradecurricular/exceptions/CursoException.java
@@ -0,0 +1,15 @@
+package com.cliente.escola.gradecurricular.exceptions;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class CursoException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    private final HttpStatus httpStatus;
+
+    public CursoException(final String mensagem, final HttpStatus httpStatus) {
+        super(mensagem);
+        this.httpStatus = httpStatus;
+    }
+}
