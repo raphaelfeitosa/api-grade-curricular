7c5bf1aff9fcbdc14a85ea3a0a9485297d6f506d
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-30 23:47:46 -0300

    create CursoEntity

---
+++ src/main/java/com/cliente/escola/gradecurricular/entities/CursoEntity.java
@@ -0,0 +1,36 @@
+package com.cliente.escola.gradecurricular.entities;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_curso")
+@Data
+@NoArgsConstructor
+public class CursoEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    @Column(name = "nome")
+    private String nome;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Column(name = "codigo")
+    private String codigo;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "materia_id")
+    private List<MateriaEntity> materias;
+}
