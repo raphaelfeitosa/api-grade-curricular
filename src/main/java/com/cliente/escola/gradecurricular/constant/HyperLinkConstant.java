e130e716c6d97fe1947e5d0324c4e972d3b85ed3
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-30 23:46:33 -0300

    create constant enums

---
+++ src/main/java/com/cliente/escola/gradecurricular/constant/HyperLinkConstant.java
@@ -0,0 +1,15 @@
+package com.cliente.escola.gradecurricular.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum HyperLinkConstant {
+    ATUALIZAR("UPDATE"),
+    EXCLUIR("DELETE"),
+    LISTAR("GET_ALL"),
+    CONSULTAR("GET");
+
+    private final String valor;
+}
