ccb475bdd97ed56336273f2d005650f61e16426c
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-30 23:19:41 -0300

    add @Bean model mapper

--- src/main/java/com/cliente/escola/gradecurricular/GradeCurricularApplication.java
+++ src/main/java/com/cliente/escola/gradecurricular/GradeCurricularApplication.java
@@ -1,10 +1,12 @@
 package com.cliente.escola.gradecurricular;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @EnableCaching
 @SpringBootApplication
@@ -13,6 +15,11 @@ public class GradeCurricularApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(GradeCurricularApplication.class);
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 
     public static void main(String[] args) {
