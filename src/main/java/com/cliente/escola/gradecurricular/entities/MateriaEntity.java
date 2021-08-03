d31dcc885b1ff9d4eda0859c17e0a69333204865
Raphael Feitosa <raphaelcs2@gmail.com>
2021-08-02 22:40:42 -0300

    update name table

--- src/main/java/com/cliente/escola/gradecurricular/entities/MateriaEntity.java
+++ src/main/java/com/cliente/escola/gradecurricular/entities/MateriaEntity.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
-@Table(name = "tb_materia")
+@Table(name = "tb_materias")
 @Data
 @NoArgsConstructor
 public class MateriaEntity implements Serializable {
