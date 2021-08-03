c83d5a00b8836591eed588d404b930da491ba152
Raphael Feitosa <raphaelcs2@gmail.com>
2021-08-02 22:33:44 -0300

    add ERRO_ID_NAO_INFORMADO

--- src/main/java/com/cliente/escola/gradecurricular/constant/MensagensConstant.java
+++ src/main/java/com/cliente/escola/gradecurricular/constant/MensagensConstant.java
@@ -13,7 +13,8 @@ public enum MensagensConstant {
     ERRO_CURSO_CADASTRADO_ANTERIORMENTE("curso já possui cadastro."),
     ERRO_CURSO_NAO_ENCONTRADO("Curso não encontrado."),
     ERRO_MATERIA_NULL("Materia(as) não pode ser vazio ou nula"),
-    ERRO_ID_INFORMADO("ID não pode ser informado na operação de cadastro.");
+    ERRO_ID_INFORMADO("ID não pode ser informado na operação de cadastro."),
+    ERRO_ID_NAO_INFORMADO("Informe o ID para alterar o cadastro");
 
     private final String valor;
 }
