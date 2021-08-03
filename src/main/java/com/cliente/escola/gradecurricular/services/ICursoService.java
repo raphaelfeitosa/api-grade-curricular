42476549e5eb1baf74a0ee2cc17ffa23dd71cb24
Raphael Feitosa <raphaelcs2@gmail.com>
2021-08-03 17:15:18 -0300

    add GetCursoDto contracts methods

--- src/main/java/com/cliente/escola/gradecurricular/services/ICursoService.java
+++ src/main/java/com/cliente/escola/gradecurricular/services/ICursoService.java
@@ -1,14 +1,15 @@
 package com.cliente.escola.gradecurricular.services;
 
 import com.cliente.escola.gradecurricular.dto.CursoDto;
+import com.cliente.escola.gradecurricular.dto.GetCursoDto;
 
 import java.util.List;
 
 public interface ICursoService {
     Boolean cadastrarCurso(final CursoDto cursoDto);
-    List<CursoDto> listarCursos();
-    CursoDto consultarCursoPorCodigo(String codigo);
-    CursoDto consultarCursoPorId(Long id);
+    List<GetCursoDto> listarCursos();
+    GetCursoDto consultarCursoPorCodigo(String codigo);
+    GetCursoDto consultarCursoPorId(Long id);
     Boolean atualizarCurso(final CursoDto cursoDto);
     Boolean excluirCurso(final Long id);
 }
