7ebc1498b5737249f221f91102fb2d97e708dfe5
Raphael Feitosa <raphaelcs2@gmail.com>
2021-08-09 22:44:45 -0300

    config swagger remove toLowerCase

--- src/main/java/com/cliente/escola/gradecurricular/controllers/CursoController.java
+++ src/main/java/com/cliente/escola/gradecurricular/controllers/CursoController.java
@@ -1,10 +1,15 @@
 package com.cliente.escola.gradecurricular.controllers;
 
+import com.cliente.escola.gradecurricular.config.SwaggerConfig;
 import com.cliente.escola.gradecurricular.constant.HyperLinkConstant;
 import com.cliente.escola.gradecurricular.dto.CursoDto;
 import com.cliente.escola.gradecurricular.dto.GetCursoDto;
 import com.cliente.escola.gradecurricular.models.Response;
 import com.cliente.escola.gradecurricular.services.ICursoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Api(tags = SwaggerConfig.CURSO)
 @RestController
 @RequestMapping("/cursos")
 public class CursoController {
@@ -21,6 +27,12 @@ public class CursoController {
     @Autowired
     private ICursoService iCursoService;
 
+    @ApiOperation(value = "Cadastrar um novo curso")
+    @ApiResponses(value = {
+            @ApiResponse(code=201, message = "Entidade criada com sucesse"),
+            @ApiResponse(code=400, message = "Erro na requisição enviada pelo cliente"),
+            @ApiResponse(code=500, message = "Erro interno no serviço")
+    })
     @PostMapping
     public ResponseEntity<Response<Boolean>> cadastrarCurso(@Valid @RequestBody CursoDto cursoDto) {
         Response<Boolean> response = new Response<>();
@@ -35,6 +47,11 @@ public class CursoController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @ApiOperation(value = "Listar todos os cursos")
+    @ApiResponses(value = {
+            @ApiResponse(code=200, message = "Lista de entidades exibidas com sucesso"),
+            @ApiResponse(code=500, message = "Erro interno no serviço"),
+    })
     @GetMapping
     public ResponseEntity<Response<List<GetCursoDto>>> listarCursos() {
         Response<List<GetCursoDto>> response = new Response<>();
@@ -45,6 +62,12 @@ public class CursoController {
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    @ApiOperation(value = "Consultar curso por id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Curso encontrado com sucesso"),
+            @ApiResponse(code = 404, message = "Curso não encontrado"),
+            @ApiResponse(code = 500, message = "Erro interno no serviço"),
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Response<GetCursoDto>> consultarCursoPorId(@PathVariable Long id) {
         Response<GetCursoDto> response = new Response<>();
@@ -59,12 +82,18 @@ public class CursoController {
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    @ApiOperation(value = "Consultar curso por código")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Curso encontrado com sucesso"),
+            @ApiResponse(code = 404, message = "Curso não encontrado"),
+            @ApiResponse(code = 500, message = "Erro interno no serviço"),
+    })
     @GetMapping("/codigo-curso")
     public ResponseEntity<Response<GetCursoDto>> consultarCursoPorCodigo(@RequestParam("codigo") String codigo) {
 
         Response<GetCursoDto> response = new Response<>();
-        GetCursoDto getCursoDto = this.iCursoService.consultarCursoPorCodigo(codigo.toLowerCase());
-        response.setData(this.iCursoService.consultarCursoPorCodigo(codigo.toLowerCase()));
+        GetCursoDto getCursoDto = this.iCursoService.consultarCursoPorCodigo(codigo);
+        response.setData(this.iCursoService.consultarCursoPorCodigo(codigo));
         response.setStatusCode(HttpStatus.OK.value());
         response.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CursoController.class).consultarCursoPorCodigo(codigo))
                 .withSelfRel());
@@ -75,6 +104,12 @@ public class CursoController {
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    @ApiOperation(value = "Excluir um curso")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Curso excluído com sucesso"),
+            @ApiResponse(code = 404, message = "Curso não encontrado"),
+            @ApiResponse(code = 500, message = "Erro interno no serviço"),
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Response<Boolean>> excluirCurso(@PathVariable Long id) {
         Response<Boolean> response = new Response<>();
@@ -87,6 +122,13 @@ public class CursoController {
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    @ApiOperation(value = "Atualizar um curso")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Curso atualizado com sucesso"),
+            @ApiResponse(code = 400, message = "Erro na requisição enviada pelo cliente"),
+            @ApiResponse(code = 404, message = "Curso não encontrado"),
+            @ApiResponse(code = 500, message = "Erro interno no serviço"),
+    })
     @PutMapping
     public ResponseEntity<Response<Boolean>> atualizarCurso(@Valid @RequestBody CursoDto cursoDto) {
         Response<Boolean> response = new Response<>();
