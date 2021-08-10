46d90c98e965eacad59aae01a48cfdee99b46565
Raphael Feitosa <raphaelcs2@gmail.com>
2021-08-10 15:01:16 -0300

    create config oauth2

---
+++ src/main/java/com/cliente/escola/gradecurricular/config/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.cliente.escola.gradecurricular.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configuracaoGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder password = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication().withUser("root").password(password.encode("root")).roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
