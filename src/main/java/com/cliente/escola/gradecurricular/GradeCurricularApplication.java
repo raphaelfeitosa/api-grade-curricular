227348e9f2ac08a9c809dc513d050d652f65d9a2
Raphael Feitosa <raphaelcs2@gmail.com>
2021-08-11 22:15:49 -0300

    update

--- src/main/java/com/cliente/escola/gradecurricular/GradeCurricularApplication.java
+++ src/main/java/com/cliente/escola/gradecurricular/GradeCurricularApplication.java
@@ -1,12 +1,10 @@
 package com.cliente.escola.gradecurricular;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 
 @EnableCaching
 @SpringBootApplication
