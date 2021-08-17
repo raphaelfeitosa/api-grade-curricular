bb6fa6fda12fd044f02e26d8c1a8fafdbdec854e
Raphael Feitosa <raphaelcs2@gmail.com>
2021-08-17 18:29:00 -0300

    allowed webjars, static, swagger

--- src/main/java/com/cliente/escola/gradecurricular/config/WebSecurityConfig.java
+++ src/main/java/com/cliente/escola/gradecurricular/config/WebSecurityConfig.java
@@ -19,7 +19,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+        String[] allowed = new String[]{
+                "/webjars", "/static/**", "/swagger*/**"
+        };
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers(allowed).permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
