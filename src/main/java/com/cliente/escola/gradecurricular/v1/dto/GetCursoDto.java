e5ff7ed2c905f0cf7879a5b06811dd604e310813
Raphael Feitosa <raphaelcs2@gmail.com>
2021-08-17 17:06:40 -0300

    remove validation

--- src/main/java/com/cliente/escola/gradecurricular/v1/dto/GetCursoDto.java
+++ src/main/java/com/cliente/escola/gradecurricular/v1/dto/GetCursoDto.java
@@ -7,8 +7,6 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.hateoas.RepresentationModel;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
@@ -18,12 +16,8 @@ public class GetCursoDto extends RepresentationModel<GetCursoDto> {
 
     private Long id;
 
-    @NotBlank(message = "Informe o nome do curso")
-    @Size(min = 10, max = 30)
     private String nome;
 
-    @NotBlank(message = "Informe o codigo do curso")
-    @Size(min = 2, max = 5)
     private String codigo;
 
     List<MateriaEntity> materias;
